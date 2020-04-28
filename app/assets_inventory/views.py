4e80e31d6b093430b1d37be4be4d6e1b7972e8de
njcx <njcx86@gmail.com>
2020-04-28 18:18:50 +0800

    init

--- app/assets_inventory/views.py
+++ app/assets_inventory/views.py
@@ -3,16 +3,5 @@
 # @Email   : njcx86@gmail.com
 
 
-from . import models
-from . import serializers
-from rest_framework_mongoengine import generics
+from flask_restplus import Resource
 
-
-class data1View(generics.ListCreateAPIView):
-    queryset = models.data1.objects.all().order_by('siteid')
-    serializer_class = serializers.data1Serializer
-
-
-class data2View(generics.ListCreateAPIView):
-    queryset = models.data2.objects.all().order_by('personid')
-    serializer_class = serializers.data2Serializer
